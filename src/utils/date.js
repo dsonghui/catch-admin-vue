9e8492ac310b8637b1df5dca6c05e860ffe0300c
david <david.deng@jcinfotech.com>
2020-06-05 15:43:50 +0800

    update date

--- src/utils/date.js
+++ src/utils/date.js
@@ -8,9 +8,9 @@ export function timestampToDate (timestamp) {
   const date = new Date(timestamp)
 
   return date.getFullYear() + '-' +
-          date.getMonth() + '-' +
-          date.getDay() + ' ' +
-          date.getHours() + '时' +
-          date.getMinutes() + '分' +
-          date.getDay() + '秒'
+    (date.getMonth() + 1) + '-' +
+    date.getDay() + ' ' +
+    date.getHours() + '时' +
+    date.getMinutes() + '分' +
+    date.getDay() + '秒'
 }
